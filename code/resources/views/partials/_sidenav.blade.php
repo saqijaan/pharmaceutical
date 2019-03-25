7887358e6747edfa0cdcfdbf8935b36a7e22a2ad
Muhammad Saqib Ali <saqiba874@gmail.com>
2019-03-25 11:34:02 +0500

    Some changes on sidebar image remaved + name changes to biomerge

--- code/resources/views/partials/_sidenav.blade.php
+++ code/resources/views/partials/_sidenav.blade.php
@@ -4,7 +4,7 @@
     <div class="col-md-3 left_col">
         <div class="left_col scroll-view">
             <div class="navbar nav_title" style="border: 0;">
-                <a href="/" class="site_title"><i class="fa fa-paw"></i> <span>Biomerge Pharamceuticals!</span></a>
+                <a href="/" class="site_title"><span>Biomerge Pharamceuticals!</span></a>
             </div>
 
             <div class="clearfix"></div>
